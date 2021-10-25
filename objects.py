27d5a71bc327f3c45ce8be51ebf85bbfb7ce4669
Eli Lowry <lowryec17@gcc.edu>
2021-10-25 18:59:46 -0400

    Here she is...a baby website

--- objects.py
+++ objects.py
@@ -1,13 +1,13 @@
 from hasher import Hasher
 import os
 
-scriptdir = os.path.dirname(__file__)
-pepfile = os.path.join(scriptdir, "pepper.bin")
+# scriptdir = os.path.dirname(__file__)
+# pepfile = os.path.join(scriptdir, "pepper.bin")
 
-with open(pepfile, 'rb') as fin:
-    pepper_key = fin.read()
+# with open(pepfile, 'rb') as fin:
+#     pepper_key = fin.read()
 
-pwd_hasher = Hasher(pepper_key)
+# pwd_hasher = Hasher(pepper_key)
 
 class User():
     pass

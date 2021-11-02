3e2a9a0f42aea4ae1782109fc6e7605b5e2e62fd
MacroGrove <71152799+MacroGrove@users.noreply.github.com>
2021-11-02 10:20:58 -0400

    Added spacing

--- forms.py
+++ forms.py
@@ -3,20 +3,24 @@ from wtforms.validators import EqualTo, InputRequired, Email
 from wtforms.fields.core import StringField
 from wtforms.fields.html5 import EmailField
 from wtforms.fields.simple import PasswordField, SubmitField
+
 class LoginForm(FlaskForm):
     email = EmailField("Enter email: ", validators=[InputRequired(), Email()])
     # username = StringField("Enter username: ", validators=[InputRequired()])
     password = PasswordField("Enter password: ", validators=[InputRequired()])
     submit = SubmitField("Submit")
+
 class RegisterForm(FlaskForm):
     username = StringField("Enter username: ", validators=[InputRequired()])
     password = PasswordField("Enter password: ", validators=[InputRequired()])
     confirm_password = PasswordField("Confirm password: ", validators=[InputRequired(), EqualTo('password')])
     email = EmailField("Enter email: ", validators=[InputRequired(), Email()])
     submit = SubmitField("Submit")
+
 class InputVidForm(FlaskForm):
     video = StringField("Enter video link: ")
     submit = SubmitField("Submit")
+
 class RoomForm(FlaskForm):
     room =  StringField("", validators=[InputRequired()])
     submit = SubmitField("Submit")

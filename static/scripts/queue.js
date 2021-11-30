cf79d0d27981d71b20062bd84e8c70cd800e6aed
CalThresh <caleb.henry.johnson@gmail.com>
2021-11-29 21:01:16 -0500

    queue works!

--- static/scripts/queue.js
+++ static/scripts/queue.js
@@ -1,16 +1,21 @@
 window.addEventListener("DOMContentLoaded", function() {
-	const queueButton = document.getElementById("queue-button")
-    queueButton.addEventListener("click", )
+
+     // on page load, load all current videos in queue
+     loadQueue();
+
+     // attach an event listener to the send button to post messages
+     const queueButton = document.getElementById("queue-button")
+     queueButton.addEventListener("click", storeQueueVideo)
 });
 
 // load messages from database
 async function loadQueue() {
-    const room_id = document.getElementById("articleID").innerText;
+    const room_id = document.getElementById("roomID").value;
     fetch(`/api/v1/queue/${room_id}`)
         .then(validateJSON)
-        .then(messageData => {
-            for (const queueVideo of queueVideoData.queue) {
-                insertMessage(queueVideo);
+        .then(queueData => {
+            for (const video of queueData.videos) {
+                insertVideo(video);
             }
         })
         .catch(error => {
@@ -18,25 +23,46 @@ async function loadQueue() {
         });
 }
 
-async function getQueue() {
-    return 
-}
-
 /**
  * Asynchronously add a div with all messagse from db to the page.
  * 
  * @param {Array.<queueVideo>} queue an array of Message objects from db
  */
-async function insertMessage(queueVideo) {
+async function insertVideo(video) {
     const chat = document.getElementById("queue-list");
     const item = document.createElement("p");
-    item.innerText = queueVideo;
+    item.innerText = video.url;
     queue.appendChild(item);
 }
 
 // send message to database
 async function storeQueueVideo() {
+    /* Steps
+        1. get the roomID and comment text from the DOM
+        2. create a new comment object to be posted
+        3. create a new POST fetch request
+        4. once POST is fulfilled, insert comment into the page
+    */
 
+    const roomID = document.getElementById("roomID").value;
+    const url = document.getElementById("queue-field").value;
+    const queueVideo = {
+        "roomID": roomID,
+        "url": url
+    };
+
+    return fetch(`/api/v1/queue/${roomID}/`, {
+        method: "POST",
+        headers: {
+            "content-Type": "application/json"
+        },
+        body: JSON.stringify(queueVideo)
+        })
+        .then(validateJSON)
+        .then(insertVideo)
+        .catch(error => {
+            console.log("Queue Post Failed: ", error)
+        });
 }
 
 /**
